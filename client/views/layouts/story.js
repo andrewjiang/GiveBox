a7a220cdcddab534d0d12afe58d4f96c4ff0ea0c
railgunlv5 <neilliang1988@gmail.com>
2014-04-06 00:15:39 -0700

    hope no break

--- client/views/layouts/story.js
+++ client/views/layouts/story.js
@@ -1,12 +1,26 @@
+if (Meteor.isClient) {
+
+
+}
+
 Template.story.helpers({ 
 	currentStory: function() {
 		var profile = Stories.findOne(Session.get("currentStoryId")); 
 		return profile;
 	},
-
-	wishlist: function() {
-		var email = Wishlist.findOne(Session.get("currentStoryId")).email;
-		var items = Wishlist.findOne({"email":email}).data;
-		return items;
-	},
+	
 });
+
+setTimeout(function(){
+	var items = Wishlist.findOne({"email":Stories.findOne(Session.get("currentStoryId")).email});
+	var html = '';
+	for(var i in items['data']){
+		var item = items['data'][i];
+		html += "<tr><td>" + item.name + "</td>";
+		html += "<td><img src='" +item.picture + "'></td>";
+		html += "</tr>";
+	}
+	$("#wishlist_table").html(html);		
+}, 2000);
+
+
--- server/parse_email.js
+++ server/parse_email.js
@@ -1,22 +1,24 @@
 
+
 if (Meteor.isServer) {
   Meteor.Router.add({
     '/parse_email':  function() {
+
+      var arrayFunc = function(array) {
+        
+
         post = this.request.body;
         text = post.text
         from = post.from
         wishlistId = text.substring(text.indexOf("wishlist") + "wishlist/".length, text.indexOf("ref") - 1);
         mail = from.substring(from.indexOf("<") + 1, from.indexOf(">"))
-        HTTP.get("http://www.justinscarpetti.com/projects/amazon-wish-lister/api/?" + wishlistId,
+        HTTP.get("http://fromneilwithlove.herokuapp.com/wishlist.php?id=1?" + wishlistId,
           {},
           function (error, result) {
             if (!error) {
             	var data = result['content'];
             	var data = JSON.parse(data);
             	var t = Wishlist.insert({"data":data, "email":mail});
-            	        console.log(t);
-
-            	//Meteor.publish('wishlist_data', data);
             }else{
             	console.log("error");
             }
