00cc1c9ead7795f2f8714817232d144318b33040
railgunlv5 <neilliang1988@gmail.com>
2014-04-05 20:03:27 -0700

    yo

--- server/parse_email.js
+++ server/parse_email.js
@@ -4,16 +4,16 @@ if (Meteor.isServer) {
     '/parse_email':  function() {
         post = this.request.body;
         text = post.text
-        
+        from = post.from
         wishlistId = text.substring(text.indexOf("wishlist") + "wishlist/".length, text.indexOf("ref") - 1);
-        
+        mail = from.substring(from.indexOf("<") + 1, from.indexOf(">"))
         HTTP.get("http://www.justinscarpetti.com/projects/amazon-wish-lister/api/?" + wishlistId,
           {},
           function (error, result) {
             if (!error) {
             	var data = result['content'];
             	var data = JSON.parse(data);
-            	var t = Wishlist.insert({"data":data});
+            	var t = Wishlist.insert({"data":data, "email":mail});
             	        console.log(t);
 
             	//Meteor.publish('wishlist_data', data);
