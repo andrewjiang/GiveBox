8747f8844422c3442c6c97f5fc0899f403c0a99b
railgunlv5 <neilliang1988@gmail.com>
2014-04-05 19:40:10 -0700

    yo

--- server/parse_email.js
+++ server/parse_email.js
@@ -3,12 +3,26 @@ if (Meteor.isServer) {
   Meteor.Router.add({
     '/parse_email':  function() {
         post = this.request.body;
-        subject = post.subject;
         text = post.text
-        html = post.html
-        var t = Mail.insert({'subject':subject, "text":text, "html":html});
+        
+        wishlistId = text.substring(text.indexOf("wishlist") + "wishlist/".length, text.indexOf("ref") - 1);
+        
+        HTTP.get("http://www.justinscarpetti.com/projects/amazon-wish-lister/api/?" + wishlistId,
+          {},
+          function (error, result) {
+            if (!error) {
+            	var data = result['content'];
+            	var data = JSON.parse(data);
+            	var t = Wishlist.insert({"data":data});
+            	        console.log(t);
 
-        console.log(t);
+            	//Meteor.publish('wishlist_data', data);
+            }else{
+            	console.log("error");
+            }
+          });
+
+
         return [200, "Success"];
      },
   });
