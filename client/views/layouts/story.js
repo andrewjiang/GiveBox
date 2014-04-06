59d40664770e4cbb8cf4027271161da7a1987e63
railgunlv5 <neilliang1988@gmail.com>
2014-04-06 00:57:58 -0700

    adding paypal

--- client/views/layouts/story.js
+++ client/views/layouts/story.js
@@ -11,6 +11,22 @@ Template.story.helpers({
 	
 });
 
+Template.paypalCreditCardForm.events({
+    'submit #paypal-payment-form': function(evt, tmp){
+      evt.preventDefault();
+
+      var card_data = Template.paypalCreditCardForm.card_data();
+
+      //Probably a good idea to disable the submit button here to prevent multiple submissions.
+
+      Meteor.Paypal.purchase(card_data, {total: '100.50', currency: 'USD'}, function(err, results){
+        if (err) console.error(err);
+        else console.log(results);
+      });
+    }
+  });
+
+
 setTimeout(function(){
 	var items = Wishlist.findOne({"email":Stories.findOne(Session.get("currentStoryId")).email});
 	var html = '';
---
+++ server/paypal_config.js
@@ -0,0 +1,6 @@
+Meteor.Paypal.config({
+    'host': 'api.sandbox.paypal.com',
+    'port': '',
+    'client_id': 'AQCZ0RB3qFy40FUOZpsVZa-fONaDX_IauUU-Y6ZYp5zJCas59d1xOChjs4VB',
+    'client_secret': 'ENo6eRCT2eZID2lK5eV819F7VJ-cXx3b5f8jgz2qHMhsE1dEDDAI3aQpHlBZ'
+  });
